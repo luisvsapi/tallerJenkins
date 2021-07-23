f1636611958efe69693f88763f2d3cd92b8d54e0
luanvill <luanvill@espol.edu.ec>
2021-07-22 19:52:14 -0500

    Agregando 2 test cases mas, con total de 6

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -15,18 +15,30 @@ public class RelationalOperatorTest {
 	@Test
 	public void isGreaterTest2() {
 		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(2, 2));
+	}
+	
+	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isGreater(2, 1));
 	}
 
 	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertFalse(tester.isLess(4, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+	
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.isLess(3, 4));
 	}
 }
